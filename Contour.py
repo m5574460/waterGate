a12c307bfc8f263345db6c3d491514f3f9b0f292
m5574460 <m5574460@yahoo.com.tw>
2020-08-11 14:04:51 +0800

    upload

---
+++ Contour.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import os
+import copy
+
+image = cv2.imread('D:/waterGate/image/7.jpg')
+clone = image.copy()
+#cv2.imshow("clone", clone)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 1)
+#blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+binaryIMG = cv2.Canny(blurred, 20, 160)
+
+ret, binary = cv2.threshold(binaryIMG.copy(), 127, 255, cv2.THRESH_BINARY)  
+(_, cnts, _) = cv2.findContours(binaryIMG.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(clone, cnts, -1, (0, 255, 0), 2)
+
+
+cnt = cnts[600]
+print(len(cnts))
+imgnew = cv2.drawContours(clone, cnt, -1, (0,255,0), 3)
+
+M = cv2.moments(cnt)
+cx = int(M['m10']/M['m00'])
+cy = int(M['m01']/M['m00'])
+area = cv2.contourArea(cnt)
+perimeter = cv2.arcLength(cnt,True)
+epsilon = 0.02*perimeter
+approx = cv2.approxPolyDP(cnt,epsilon,True)
+imgnew1 = cv2.drawContours(clone, approx, -1, (0,0,255), 3)
+imgnew1 = cv2.circle(clone, (cx, cy), 5, (0, 0, 255), -1)
+
+print(cx, cy, area)
+
+cv2.imshow("new", imgnew)
+cv2.imshow("new1", imgnew1)
+cv2.imshow("bin", binaryIMG)
+cv2.imshow("blu", blurred)
+cv2.imshow("gray", gray)
+cv2.imshow("clone", clone)
+cv2.waitKey(0)
---
+++ HoughLine.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+# Input image
+img = cv2.imread('./image/8.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Needed due to JPG artifacts
+_, temp = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
+
+# Dilate to better detect contours
+temp = cv2.dilate(temp, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
+
+# Find largest contour
+_, cnts, _ = cv2.findContours(temp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+largestCnt = []
+for cnt in cnts:
+    if len(cnt) > len(largestCnt):
+        largestCnt = cnt
+
+# Determine center of area of largest contour
+M = cv2.moments(largestCnt)
+x = int(M["m10"] / M["m00"])
+y = int(M["m01"] / M["m00"])
+
+# Initiale mask for flood filling
+width, height = temp.shape
+mask = img2 = np.ones((width + 2, height + 2), np.uint8) * 255
+mask[1:width, 1:height] = 0
+
+# Generate intermediate image, draw largest contour, flood filled
+temp = np.zeros(temp.shape, np.uint8)
+temp = cv2.drawContours(temp, largestCnt, -1, 255, cv2.FILLED)
+_, temp, mask, _ = cv2.floodFill(temp, mask, (x, y), 255)
+temp = cv2.morphologyEx(temp, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
+
+# Count pixels in desired region
+area = cv2.countNonZero(temp)
+
+# Put result on original image
+img = cv2.putText(img, str(area), (x, y), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, 255)
+
+cv2.imshow('Input', img)
+cv2.imshow('Temp image', temp)
+
+cv2.waitKey(0)
